a791397127a7ec99603e9903175b2ece249fa3b1
geb-braid <maozn@hotmail.com>
2023-10-10 10:03:47 +0700

    Add more integration tests

--- tp24-api.Tests/IntegrationTests/ReceivablesControllerIntegrationTests.cs
+++ tp24-api.Tests/IntegrationTests/ReceivablesControllerIntegrationTests.cs
@@ -5,10 +5,10 @@ using tp24_api.Models;
 
 namespace tp24_api.IntegrationTests;
 
-public class ReceivablesControllerIntegrationTests : IDisposable
+public class ReceivablesControllerIntegrationTests : IClassFixture<CustomWebApplicationFactory>, IDisposable
 {
-    private CustomWebApplicationFactory _factory;
-    private HttpClient _client;
+    private readonly CustomWebApplicationFactory _factory;
+    private readonly HttpClient _client;
 
     public ReceivablesControllerIntegrationTests()
     {
@@ -25,10 +25,7 @@ public class ReceivablesControllerIntegrationTests : IDisposable
     [Fact]
     public async Task Get_what_was_Posted()
     {
-        using var response = await _client.PostAsJsonAsync("api/receivables", Receivables.CLOSED_USD);
-        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
-
-        var createdReceivable = await response.Content.ReadFromJsonAsync<Receivable>();
+        var createdReceivable = await PostReceivable(Receivables.CLOSED_USD);
         Assert.NotNull(createdReceivable?.Id);
 
         var returnedReceivable = await _client.GetFromJsonAsync<Receivable>($"api/receivables/{createdReceivable.Id}");
@@ -37,20 +34,86 @@ public class ReceivablesControllerIntegrationTests : IDisposable
         Assert.Equal(Receivables.CLOSED_USD.ClosedDate, returnedReceivable.ClosedDate);
     }
 
+    [Fact]
+    public async Task Get_Summary()
+    {
+        await PostReceivable(Receivables.CLOSED_USD);
+        await PostReceivable(Receivables.CLOSED_USD);
+        await PostReceivable(Receivables.CLOSED_USD);
+        await PostReceivable(Receivables.CLOSED_GBP);
+        await PostReceivable(Receivables.OPEN_USD);
+        await PostReceivable(Receivables.OPEN_GBP);
+        await PostReceivable(Receivables.CANCELED);
+
+        var report = await _client.GetFromJsonAsync<ReceivablesReport>($"api/receivables?startDate=2023-09-09T22%3A08%3A15.367Z");
+        Assert.NotNull(report.Receivables);
+        Assert.Equal(6, report.Receivables.Count()); // 6 non-canceled receivables
+        Assert.Equal(new Stats(Total: 30m, Count: 3, Max: 10m), report.Summary["Closed"]["USD"]);
+        Assert.Equal(new Stats(Total: 10m, Count: 1, Max: 10m), report.Summary["Closed"]["GBP"]);
+        Assert.Equal(new Stats(Total: 10m, Count: 1, Max: 10m), report.Summary["Open"]["USD"]);
+        Assert.Equal(new Stats(Total: 10m, Count: 1, Max: 10m), report.Summary["Open"]["GBP"]);
+    }
+
+    [Fact(Skip = "Maybe fails because of: 'The EF-Core in-memory database provider can be used for limited and basic testing, however the SQLite provider is the recommended choice for in-memory testing.'")]
+    public async Task Put_Updates()
+    {
+        var receivable = await PostReceivable(Receivables.CANCELED);
+        Assert.NotNull(receivable?.Id);
+        Assert.True(receivable.Cancelled);
+
+        receivable.Cancelled = false;
+
+        using var response = await _client.PutAsJsonAsync($"api/receivables/{receivable.Id}", receivable);
+        Assert.NotNull(response);
+        Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+
+        var returnedReceivable = await _client.GetFromJsonAsync<Receivable>($"api/receivables/{receivable.Id}");
+        Assert.NotNull(returnedReceivable);
+        Assert.False(returnedReceivable.Cancelled);
+    }
+
+    [Fact]
+    public async Task Delete_Removes()
+    {
+        var receivable = await PostReceivable(Receivables.OPEN_USD);
+        Assert.NotNull(receivable?.Id);
+
+        using var response1 = await _client.DeleteAsync($"api/receivables/{receivable.Id}");
+        using var response2 = await _client.DeleteAsync($"api/receivables/{receivable.Id}");
+
+        Assert.NotNull(response1);
+        Assert.NotNull(response2);
+        Assert.Equal(HttpStatusCode.NoContent, response1.StatusCode);
+        Assert.Equal(HttpStatusCode.NoContent, response2.StatusCode);
+    }
+
+    private async Task<Receivable?> PostReceivable(Receivable receivable)
+    {
+        using var response = await _client.PostAsJsonAsync("api/receivables", receivable);
+        Assert.NotNull(response);
+        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+
+        return await response.Content.ReadFromJsonAsync<Receivable>();
+    }
+
     public static class Receivables
     {
-        public static readonly Receivable CLOSED_USD = CLOSED("USD");
-        public static readonly Receivable CLOSED_GBP = CLOSED("GBP");
+        public static readonly Receivable OPEN_USD = Receivable("USD");
+        public static readonly Receivable OPEN_GBP = Receivable("GBP");
+        public static readonly Receivable CLOSED_USD = Receivable("USD", closed: true);
+        public static readonly Receivable CLOSED_GBP = Receivable("GBP", closed: true);
+        public static readonly Receivable CANCELED = Receivable("USD", cancelled: true);
 
-        public static Receivable CLOSED(string currencyCode) => new()
+        public static Receivable Receivable(string currencyCode, bool closed = false, bool cancelled = false) => new()
         {
             Reference = "reference",
             CurrencyCode = currencyCode,
             IssueDate = ParseIso8601("2023-10-06T07:05:20.257Z"),
-            OpeningValue = 9,
-            PaidValue = 4,
+            OpeningValue = 11,
+            PaidValue = 1,
             DueDate = ParseIso8601("2023-10-06T07:05:20.257Z"),
-            ClosedDate = ParseIso8601("2023-10-06T07:05:20.257Z"),
+            ClosedDate = closed ? ParseIso8601("2023-10-06T07:05:20.257Z") : null,
+            Cancelled = cancelled,
             DebtorName = "debtorName",
             DebtorReference = "debtorReference",
             DebtorAddress1 = "debtorAddress1",
@@ -62,7 +125,7 @@ public class ReceivablesControllerIntegrationTests : IDisposable
             DebtorRegistrationNumber = "debtorRegistrationNumber"
         };
 
-        private static DateTime ParseIso8601(string iso8601)
-            => DateTime.Parse(iso8601, CultureInfo.InvariantCulture, DateTimeStyles.RoundtripKind);
+        private static DateTime ParseIso8601(string iso8601String)
+            => DateTime.Parse(iso8601String, CultureInfo.InvariantCulture, DateTimeStyles.RoundtripKind);
     }
 }
